ddedf253519bbec38ff29dc71aea780c6a7fa2fc
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2023-08-28 16:17:43 +0200

    answer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printDayOfWeek(0);
+        printDayOfWeek(1);
+        printDayOfWeek(2);
+        printDayOfWeek(3);
+        printDayOfWeek(4);
+        printDayOfWeek(5);
+        printDayOfWeek(6);
+        printDayOfWeek(7);
+    }
+
+    public static void printDayOfWeek(int day){
+        String dayOfTheWeek = switch (day){
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+        System.out.println("Day " + day + " is a " + dayOfTheWeek);
     }
 }
